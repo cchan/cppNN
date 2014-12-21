e3a425b48003ca865d46fee52fec487d12850065
Clive C <doobahead@gmail.com>
2014-12-21 11:18:21 -0500

    Going to start simulating the FTC game now.

---
+++ RoboSim/RoboSim.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+//12'x12'x12' field
+//whatever stuff in the middle
+//so many various balls
+//dayum physics, is there a physics and display library already?
+//not a state function
+
+//FIRST how do we do graphics?
+
+//Use Bullet and Boost.
+
+
+
+int main(){
+
+}
+
