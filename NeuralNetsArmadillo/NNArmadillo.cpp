efdeb1184c43aad122521447393b37a1a80588b9
Clive C <doobahead@gmail.com>
2015-05-07 23:58:52 -0400

    Trying Armadillo matrix lib

---
+++ NeuralNetsArmadillo/NNArmadillo.cpp
@@ -0,0 +1,9 @@
+//http://choorucode.com/2013/11/15/how-to-use-armadillo-on-windows/
+//https://stackoverflow.com/questions/4066405/when-to-use-the-visual-studio-additional-dependencies
+//internet is awesome
+
+#include "C:\Users\Clive\Desktop\ml-res\armadillo-5.100.1\include\armadillo"
+
+int main(){
+	
+}
